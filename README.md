b612c20a800817c6127b7889aecec83f6a2a57cd
Abhishek Shekhar <abhishek.shekhar1@amdocs.com>
2018-04-23 21:59:46 +0530

    Align with PEP8 Guidelines

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movie Trailer Website
 
-You can download this code
+Python Script creates a HTML page which displays some movies. Trailers of the movie can be played by clicking on the movie tile.
+
+## Requirements
+
+You will need Python 3.6.0 Installed
+
+## Download the source code
+
 <code>git clone https://github.com/abhishek-12355/ud036_StarterCode.git</code>
 or simply click Download Zip under Clone or Download Menu at https://github.com/abhishek-12355/ud036_StarterCode/ 
 
-To Run execute
-<code>python entertainment_center.py.py</code>
+## Instructions
+
+1. Open your python shell
+2. Change directory to the source folder
+3. Run entertainment_center.py file
+<code>python entertainment_center.py</code>
+4. Enjoy!
--- media.py
+++ media.py
@@ -1,9 +1,11 @@
-
-
 class Movie(object):
-	"""Movie class is a representation of a movie's information. It stores the movie's title, poster url and youtube trailer url"""
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
-		self.title=title
-		self.poster_image_url=poster_image_url
-		self.trailer_youtube_url=trailer_youtube_url
-		
+    """Movie class is a representation of a movie's information.
+    Attributes:
+        title (str): Name of the movie
+        post_image_url: Url of the poster image
+        trailer_youtube_url: Url of the youtube trailer video
+    """
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
